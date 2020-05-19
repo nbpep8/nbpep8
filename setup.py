da41b54b324c5130fab7ff6506d3f7fa4ce0d556
Debanga R. Neog, Ph.D <debanga88@gmail.com>
2020-05-19 03:28:25 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -18,6 +18,6 @@ setup(
     install_requires=requirements,
     classifiers=[
         "Programming Language :: Python :: 3.7",
-        "License :: MIT",
+        "License :: OSI Approved :: MIT License",
     ],
 )
