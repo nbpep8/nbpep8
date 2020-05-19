4032cb8d2b2acbd87783cca00e4ca9c5cf38fcc3
Debanga R. Neog, Ph.D <debanga88@gmail.com>
2020-05-19 03:08:50 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 with open("README.md", "r") as readme_file:
     readme = readme_file.read()
 
-requirements = ["ipython>=6", "nbformat>=4", "nbconvert>=5", "requests>=2"]
+requirements = ["ipython>=7.13.0", "nbformat>=4", "nbconvert>=5", "requests>=2"]
 
 setup(
     name="nbpep8",
