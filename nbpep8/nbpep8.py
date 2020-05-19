d1301aa048e8d2a874a6b451b020b6563de93d42
Debanga R. Neog, Ph.D <debanga88@gmail.com>
2020-05-19 01:42:22 -0400

    Update nbpep8.py

--- nbpep8/nbpep8.py
+++ nbpep8/nbpep8.py
@@ -1,1 +1,12 @@
-
+def pep8():
+    """ Display PEP8 analysis for a notebook cell
+    Usage:
+        - pep8() at the end of the notebook code cell
+    Returns:
+        - PEP8 analysis
+    """
+    text_file = open("cell_content.py", "w")
+    n = text_file.write(_ih[-1][:] + '\n')
+    text_file.close()
+    !pycodestyle cell_content.py
+    !rm cell_content.py 
