9038f088a6cf2f98acdcc9e08afdd01448cb3980
Debanga R. Neog, Ph.D <debanga88@gmail.com>
2020-05-19 02:55:08 -0400

    Update nbpep8.py

--- nbpep8/nbpep8.py
+++ nbpep8/nbpep8.py
@@ -1,4 +1,4 @@
-def pep8():
+def pep8(content: str) -> None:
     """ Display PEP8 analysis for a notebook cell
     Usage:
         - pep8() at the end of the notebook code cell
@@ -7,7 +7,7 @@ def pep8():
     """
     global _ih
     text_file = open("cell_content.py", "w")
-    n = text_file.write(_ih[-1][:] + '\n')
+    n = text_file.write(content[-1][:] + '\n')
     text_file.close()
     s = subprocess.run(["pycodestyle", "cell_content.py"], capture_output=True)
     print(s.stdout.decode('utf8'))
